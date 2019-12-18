d2339ada3801ffd3de44de4b9551cd52de6ad26e
Eden Darnige <45365480+edarnige@users.noreply.github.com>
2019-12-18 11:44:19 +0100

    Create exos.py

---
+++ exos.py
@@ -0,0 +1,21 @@
+#######
+# Modé́lisation d́e Réseaux Métaboliques
+# MOCELL 2019
+# Eden Darnige, Juan Manuel Garciam Théo Gauvrit 
+######
+
+#Une seconde analyse de flux (diapo 27)
+
+#Analyse de la distribution des flux (diapo 28)
+
+#Analyse de la variabilité́ des flux (diapo 29)
+
+#Des KOs artificiels (diapo 30)
+
+#Analyse de productibilité de molécules (diapos 31-32)
+
+#Croissance sur différents substrats (diapo 35)
+
+#Des modèles plus gros (diapo 37)
+
+#Un peu de bioengineering (diapo 38)
