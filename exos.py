240827c665c1dd9fcb9968a2508d2c8aba25602f
Eden Darnige <45365480+edarnige@users.noreply.github.com>
2019-12-18 11:59:41 +0100

    Croissance sur différents substrats (diapo 35)

--- exos.py
+++ exos.py
@@ -23,6 +23,26 @@ import matplotlib.pyplot as plt
 #Analyse de productibilité de molécules (diapos 31-32)
 
 #Croissance sur différents substrats (diapo 35)
+print("\nCroissance sur differents substrats")
+
+#Import test model included in cobra
+modelTextbook = cobra.test.create_test_model("textbook")
+
+#Production of "ac" in function of O2 consumption 
+prod_env = production_envelope(modelTextbook, 
+    reactions = ["EX_o2_e"], objective="EX_ac_e",
+    carbon_sources = "EX_glc__D_e")
+
+prod_env2 = production_envelope(modelTextbook,
+    ["EX_glc__D_e","EX_o2_e"])
+
+print(prod_env)
+print(prod_env2)
+
+prod_env.plot(
+    kind='line', x='EX_o2_e', y='carbon_yield_maximum');
+
+plt.show()
 
 #Des modèles plus gros (diapo 37)
 
